5988d1907370fb337163fee006b7de4ea112dac1
Regie S. Baquero <regiebaquero@gmail.com>
2020-12-16 11:57:32 +0800

    Add project files.

---
+++ dotnet_webgrid/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using dotnet_webgrid.Models;
+
+namespace dotnet_webgrid.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly AppDBContext _appDBContext;
+
+        public HomeController(ILogger<HomeController> logger, AppDBContext appDBContext)
+        {
+            _logger = logger;
+            _appDBContext = appDBContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public JsonResult getAllUsers()
+        {
+
+            List<User> users = new List<User>();
+            users = _appDBContext.users.Select(c => c).ToList();
+
+            return Json(new { data = users });
+        }   
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ dotnet_webgrid/Models/AppDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_webgrid.Models
+{
+    public class AppDBContext: DbContext
+    {
+        private readonly DbContextOptions _options;
+
+        public AppDBContext(DbContextOptions options) : base(options)
+        {
+            _options = options;
+        }
+
+        public DbSet<User> users { get; set; }
+    }
+}
